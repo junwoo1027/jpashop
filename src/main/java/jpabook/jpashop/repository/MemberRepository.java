90a4e793d9c20e84d3df7648fa6caa3d1b2770b3
junwoo1027 <38120537+junwoo1027@users.noreply.github.com>
2019-12-03 00:03:19 +0900

    회원 도메인 개발 완료

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    //회원 정보 저장
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    //회원 조회
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    //회원정보 리스트 출력
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
